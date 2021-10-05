36c4f71d7bc848a8341d40d4b9bf8c38e5977bbb
kmorgport <kmorgan0057@gmail.com>
2021-10-05 16:38:14 -0500

    creating auth form

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Grid } from '@material-ui/core'
+import useStyles from './styles'
+
+const Auth = () => {
+    const state = null
+    return (
+        <div>AUTH</div>
+    )
+}
+
+export default Auth
