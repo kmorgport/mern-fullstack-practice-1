4957ead38f077cf8e320ea9eff70049847a63da2
kmorgport <kmorgan0057@gmail.com>
2021-10-21 10:03:21 -0500

    user routes

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,19 @@
 import { AUTH } from '../constants/actionTypes';
 import * as api from '../api'
 
-export const signIn = () => async (dispatch)=> {
-
+export const signIn = (formData, history) => async (dispatch)=> {
+    try{
+        //login the user
+        //use history to navigate to the home page
+    }catch (error){
+        console.log(error)
+    }  
 }
 
 export const signUp = () => async (dispatch) => {
-    
-}
+    try{
+        //sign up the user
+    }catch(error){
+        console.log(error)
+    }
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors())
 //routes must come AFTER app.use(cors())
 app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 
 const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.r7gfx.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
