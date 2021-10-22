535495672bc8ec1071b4c92c2817ac8213b4b0fd
kmorgport <kmorgan0057@gmail.com>
2021-10-22 10:17:56 -0500

    made first middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+
+
+
+const auth = async (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+
+        let decodedData;
+        if(token && isCustomAuth ){
+            //second argument will  be the same secret/env variable
+            decodedData = jwt.verify(token, 'test');
+            req.userId = decodedData?.id
+        }else{
+            decodedData = jwt.decode(token)
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export default auth
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/', getPosts )
-router.post('/', createPost)
+router.post('/', auth, createPost)
 router.patch('/:id', updatePost)
 router.delete('/:id', deletePost)
 router.patch('/:id/likePost', likePost)
