f6f0c363e027fd560b9d4f201a2fd964497eac37
kmorgport <kmorgan0057@gmail.com>
2021-10-25 10:48:41 -0500

    post liking auth

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -12,6 +12,19 @@ import { deletePost, likePost } from '../../../actions/post'
 const Post = ({post}) => {
     const dispatch = useDispatch()
     const classes = useStyles()
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    const Likes = ()=>{
+        if (post.likes.length > 0){
+            return post.likes.find( like => like === (user?.result?.googleId || user?.result?._id))
+                ? (
+                    <><ThumbUpAltIcon fontSize="small"/>&nbsp;{post.likes.length > 2? `You and ${post.likes.length-1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's': ''}`}</>
+                ):(
+                    <><ThumbUpAltOutlined fontSize="small">&nbsp;{post.likes.length}{post.likes.length === 1 ? 'Like': 'Likes'}</ThumbUpAltOutlined></>
+                )
+        }
+        return <><ThumbUpAltOutlined fontSize="small"/>&nbsp;like</>
+    }
     
     return(
         <Card className={classes.card}>
@@ -19,14 +32,16 @@ const Post = ({post}) => {
                 image={post.selectedFile}
                 title={post.title}/>
             <div className={classes.overlay}>
-                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
+            {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator)&&(
             <div className={classes.overlay2}>
                 <Button style={{color:'white'}} size="small" onClick={()=>{}}>
                     <MoreHorizIcon fontSize="default"/>
                 </Button>
             </div>
+            )}
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>`#${tag} `)}</Typography>
             </div>
@@ -34,15 +49,16 @@ const Post = ({post}) => {
                 <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}>
+                <Button size="small" color="primary" disabled={!user?.result} onClick={()=>dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small"/>
-                    &nbsp; Like &nbsp;
-                    {post.likeCount}
+                    <Likes/>
                 </Button>
-                <Button size="small" color="primary" onClick={()=> dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small"/>
-                    Delete
-                </Button>
+                {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator)&&(
+                                <Button size="small" color="primary" onClick={()=> dispatch(deletePost(post._id))}>
+                                    <DeleteIcon fontSize="small"/>
+                                    Delete
+                                </Button>
+                )}
             </CardActions>
         </Card>
     )
