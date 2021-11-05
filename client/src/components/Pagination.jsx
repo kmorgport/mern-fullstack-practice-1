5e9d71bec38e4d1221ed895542ded08bcecf7ac2
kmorgport <kmorgan0057@gmail.com>
2021-11-05 10:56:02 -0500

    pagination start

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
-import  {Pagination, PaginationItem} from '@material-ui/lab'
-import styles from './Posts/styles'
+import  {Pagination, PaginationItem } from '@material-ui/lab'
+import useStyles from './styles'
+
+const Paginate = () => {
+    const classes = useStyle();
+    
+    return (
+        <Pagination
+            classes= {{ul: classes.ul }}
+            count = {5}
+            page = {1}
+            variant = "outlined"
+            color = "primary"
+            renderItem = { (item)=> (
+                <PaginationItem {...item} component={Link} to={`/posts?page=${1}`}/>
+            )}
+        />
+    )
+}
+
+export default Paginate
