981e38bb731a12212b1fcbbc9bc089661d2e03d7
kmorgport <kmorgan0057@gmail.com>
2021-11-11 17:19:59 -0600

    adding Loading functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,15 +1,17 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, FETCH_BY_SEARCH} from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, START_LOADING, END_LOADING, FETCH_BY_SEARCH} from '../constants/actionTypes';
 import * as api from '../api'
 
 export const getPosts = (page) => async (dispatch) => {
 
     try{
+        dispatch({ type: START_LOADING})
         const { data } = await api.fetchPosts(page);
 
         dispatch({
             type: FETCH_ALL,
             paylaod: data
         })
+        dispatch({ type: END_LOADING })
     }catch(error){
         console.log(error.message)
     }
@@ -17,11 +19,13 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery)=> async (dispatch) => {
     try{
+        dispatch({ type: START_LOADING})
         const { data: {data} } = await api.fetchPostsBySearch(searchQuery);
         dispatch({
             type: FETCH_BY_SEARCH,
             payload: data
         })
+        dispatch({ type: END_LOADING })
     }catch(error){
         console.log(error)
     }
@@ -29,8 +33,10 @@ export const getPostsBySearch = (searchQuery)=> async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
+        dispatch({ type: START_LOADING})
         const {data} = await api.createPost(post);
         dispatch({ type: CREATE, payload: data })
+        dispatch({ type: END_LOADING })
     } catch (error) {
         console.log(error)
     }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,18 @@
 import Posts from "../components/Posts/Posts";
-import { DELETE, FETCH_ALL, CREATE, UPDATE, FETCH_BY_SEARCH} from '../constants/actionTypes';
+import { DELETE, FETCH_ALL, CREATE, UPDATE, FETCH_BY_SEARCH, START_LOADING, END_LOADING} from '../constants/actionTypes';
 //state in this case will be the posts 
-export default (state = [], action) => {
+export default (state = {isLoading: true, posts: []}, action) => {
     switch(action.type){
+        case START_LOADING:
+            return {...state, isLoading: true}
+        case END_LOADING:
+            return { ...state, isLoading: false}
         case DELETE:
-            return state.filter(post=> post._id!== action.payload)
+            return {...state, posts: state.posts.filter(post=> post._id!== action.payload)}
         case FETCH_BY_SEARCH:
             return {
                 ...state,
-                posts: action.payload
+                posts: action.payload.data
             }
         case FETCH_ALL:
             return {
@@ -17,10 +21,12 @@ export default (state = [], action) => {
                 currentPage: action.payload.currentPage,
                 numberOfPages: action.payload.numberOfPages
             }
+        case LIKE:
+            return {...state, posts: state.posts.map((post)=>(post._id === action.payload._id ? action.payload : post)) };
         case CREATE:
-            return [...state, action.payload];
+            return {...state, posts: [...state.posts, action.payload]};
         case UPDATE:
-            return state.map((post)=> post._id === action.payload._id ? action.payload : post)
+            return {...state, posts: state.posts.map((post)=> post._id === action.payload._id ? action.payload : post)}
         default:
             return state
     }
