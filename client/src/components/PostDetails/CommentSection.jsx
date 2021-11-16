49e43cc31fe1a1843542973d53e6fe13989dad96
kmorgport <kmorgan0057@gmail.com>
2021-11-16 09:29:40 -0600

    adding comment buttons

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -6,17 +6,37 @@ import useStyles from './styles';
 import { mergeClasses } from '@material-ui/styles';
 
 const CommentSection = ({post}) => {
+    const classes = useStyles()
     const [comments, setComments ] = useState();
+    const [comment, setComment ] = useState("")
+
+    const handleClick = ()=> {
+
+    }
     return (
         <div>
             <div className={classes.commentsOuterSection}>
                 <div className={classes.commentsInnerSection}>
-                    <Typography gutterBottom variant="h6"></Typography>
+                    <Typography gutterBottom variant="h6">Comments</Typography>
                     {comments.map((c,i)=>(
                         <Typography key={i} gutterBottom variant="subtitle1">
                             Comment {i}
                         </Typography>
                     ))}
+                </div>
+                <div style={{width: '70%'}}>
+                <Typography gutterBottom variant="h6">Write a comment</Typography>
+                <TextField 
+                    fullWidth 
+                    rows={4} 
+                    variant="outlined" 
+                    label="Comment" 
+                    multiline
+                    value={comment}
+                    onChange={(e)=> setComment(e.target.value)}/>
+                <Button style={{ marginTop: '10px'}} fullWidth disabled={!comment} variant="contained" onClick={handleClick}>
+                        
+                </Button>
                 </div>
             </div>
         </div>
